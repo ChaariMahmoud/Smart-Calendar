0b6318bb47ca826ba360d26053fb8485c9297685
ChaariMahmoud <chaarimahmoud55@gmail.com>
2024-06-24 12:19:05 +0200

    flutter calendar part 1

--- calendar/lib/ui/home_page.dart
+++ calendar/lib/ui/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:calendar/services/notification_services.dart';
 import 'package:calendar/services/theme_service.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,36 +13,39 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var notifyHelper;
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    notifyHelper=NotifyHelper();
+    notifyHelper = NotifyHelper();
+    notifyHelper.initializeNotification();
     notifyHelper.requestIOSPermissions();
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
-        body:const Column(children: [
-          Text("theme data",style: TextStyle(fontSize: 30))
-        ],)
-      ,
+      body: const Column(
+        children: [Text("theme data", style: TextStyle(fontSize: 30))],
+      ),
     );
   }
-  AppBar _appBar(){
-   return AppBar(
-    elevation: 0,
-    leading: GestureDetector(
-      onTap: (){
-       ThemeService().switchTheme();
-      },
-      child:  const Icon(Icons.nightlight_round ,size: 20),
-    ),
-    actions: const [
-      Icon(Icons.person ,size: 20),
-      SizedBox(width: 20)
-    ],
-  );
-}
-}
 
+  AppBar _appBar() {
+    return AppBar(
+      elevation: 0,
+      leading: GestureDetector(
+        onTap: () {
+          ThemeService().switchTheme();
+          notifyHelper.displayNotification(
+              title: "Theme changed",
+              body: Get.isDarkMode
+                  ? "Light Theme Activated"
+                  : "Dark Theme Activated");
+          notifyHelper.scheduledNotification();
+        },
+        child: const Icon(Icons.nightlight_round, size: 20),
+      ),
+      actions: const [Icon(Icons.person, size: 20), SizedBox(width: 20)],
+    );
+  }
+}
